415749b10558a24fb49c95376e9772392d0b4917
Kel-Vin6 <bron28240@gmail.com>
2026-04-02 01:33:01 +0000

    Initial Commit

---
+++ VTA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VTA
+//
+//  Created by Kelvin Wallace on 02/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VTA/VTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  VTAApp.swift
+//  VTA
+//
+//  Created by Kelvin Wallace on 02/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct VTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
