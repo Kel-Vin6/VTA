0d736af7ff3dbe7bffb1b7565bc42f4d1dc3a788
Kel-Vin6 <bron28240@gmail.com>
2026-05-11 04:14:49 +0000

    dd

---
+++ VTA/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  VTA
+//
+//  Created by Kelvin Wallace on 02/04/2026.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
---
+++ VTA/SubscriptionManager.swift
@@ -0,0 +1,8 @@
+//
+//  SubscriptionManager.swift
+//  VTA
+//
+//  Created by Kelvin Wallace on 02/04/2026.
+//
+
+import Foundation
